57494b3882b93f7dd464e9808fe79c5d3ff56f11
John <johnhamborsky94@gmail.com>
2022-03-23 15:37:57 -0400

    generated variable password and for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,18 @@
-// Assignment code here
+
 var generateBtn = document.querySelector("#generate");
+
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1-rand) + rand*max)
+}
+
+function getRandomItem(list) {
+  return list[randomInt(list.length)]
+}
 
 function generatePassword() {
 
@@ -17,17 +30,21 @@ function generatePassword() {
     return
   }
 
+  
+
+  // provide options for types of characters used in generated password 
   var userNumbers = window.confirm("Would you like your password to contain numbers?")
   var userSymbols = window.confirm("Would you like your password to contain symbols?")
   var userLowercase = window.confirm("Would you like your password to contain lowercase letters?")
   var userUppercase = window.confirm("Would you like your password to contain uppercase letters?")
 
+  // list characters to possibly be used in generated password
   var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var symbolCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "-"]
   var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-  var characterOptionsSelected = [numberCharacters]
+  var characterOptionsSelected = []
 
   if (userNumbers === true) {
     characterOptionsSelected.push(numberCharacters)
@@ -45,6 +62,19 @@ function generatePassword() {
     characterOptionsSelected.push(uppercaseCharacters)
   }
 
+  if (characterOptionsSelected.length === 0) {
+    characterOptionsSelected.push(uppercaseCharacters)
+  }
+
+  var generatedPassword = ""
+
+  for (var i = 0; i < passwordCharacterLength; i++) {
+    var randomList = getRandomItem(characterOptionsSelected)
+    var randomCharacter = getRandomItem(randomList)
+    generatedPassword += randomCharacter
+  }
+
+  return generatedPassword
 
 }
 
