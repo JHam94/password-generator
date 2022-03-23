beff02dbf5ff019f1188ba67c780b679d4425b24
John <johnhamborsky94@gmail.com>
2022-03-23 14:19:19 -0400

    add character options for password and if statements

--- Develop/script.js
+++ Develop/script.js
@@ -17,7 +17,37 @@ function generatePassword() {
     return
   }
 
+  var userNumbers = window.confirm("Would you like your password to contain numbers?")
+  var userSymbols = window.confirm("Would you like your password to contain symbols?")
+  var userLowercase = window.confirm("Would you like your password to contain lowercase letters?")
+  var userUppercase = window.confirm("Would you like your password to contain uppercase letters?")
+
+  var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "-"]
+  var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+  var characterOptionsSelected = [numberCharacters]
+
+  if (userNumbers === true) {
+    characterOptionsSelected.push(numberCharacters)
+  } 
+
+  if (userSymbols === true) {
+    characterOptionsSelected.push(symbolCharacters)
+  }
+
+  if (userLowercase === true) {
+    characterOptionsSelected.push(lowercaseCharacters)
+  }
+
+  if(userUppercase === true) {
+    characterOptionsSelected.push(uppercaseCharacters)
+  }
+
+
 }
+
 
 // Write password to the #password input
 function writePassword() {
