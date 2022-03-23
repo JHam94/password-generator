f93f939f991f38381b36459e944b68024408f79d
John <johnhamborsky94@gmail.com>
2022-03-23 13:13:37 -0400

    window prompt/alert and if statements for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,23 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+  var userInput = window.prompt("How many characters would you like your password to be?")
+
+  var passwordCharacterLength = parseInt(userInput)
+
+  if (isNaN(passwordCharacterLength)) {
+    window.alert("That is not a number!")
+    return
+  } 
+
+  if (passwordCharacterLength < 8 || passwordCharacterLength > 128) {
+    window.alert("Password must be between 8 and 128 characters in length")
+    return
+  }
+
+}
 
 // Write password to the #password input
 function writePassword() {
